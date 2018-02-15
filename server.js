f378c0415c8088cac1c13c60da79731f4f354421
Stuart Kushner <stuartkushner@Stuarts-MacBook-Pro.local>
2018-02-14 21:14:50 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the express app
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Use models for syncing
+var db = require("./models");
+
+// Sets up express app to handle data parsing
+
+// Static directory
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/api-routes.js")(app);
+require("./routes/http-routes.js")(app);
+
+// Syncing our sequelize model and starting our app
+db.sequelize.sync({ force: true }).then(function(){
+	app.listen(PORT, function(){
+		console.log("App listening on PORT " + PORT);
+	});
+});
